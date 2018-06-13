584ea966e8c8a26f267d373329400751af04a26a
teasacura <teasacuraster@gmail.com>
2018-06-13 17:37:19 -0400

    updated except for delete

--- src/index.js
+++ src/index.js
@@ -11,30 +11,29 @@ function init() {
 
   document.addEventListener('submit', (e) =>{
     e.preventDefault()
-    console.log(e.target.id)
     if(e.target.id === "create-task-form"){
       const listId = document.querySelector("option:checked").dataset.listId
       const newTaskDescription = document.querySelector("#new-task-description").value
       const newTaskPriority = document.querySelector("#new-task-priority").value
       let selectedList = List.find(listId);
+      renderTask(selectedList, newTaskDescription, newTaskPriority)
 
-      // render/create new Task
-      const tasksUl = document.querySelector(`[data-ul-id="${selectedList.id}"]`)
-      const newTask = new Task(selectedList, newTaskDescription, newTaskPriority)
-      const newTaskHTML = newTask.taskHTML()
-      tasksUl.innerHTML += newTaskHTML
     }
   })
 }
 
+function renderTask(selectedList, newTaskDescription, newTaskPriority) {
+  const tasksUl = document.querySelector(`[data-ul-id="${selectedList.id}"]`)
+  const newTask = new Task(selectedList, newTaskDescription, newTaskPriority)
+  const newTaskHTML = newTask.taskHTML()
+  tasksUl.innerHTML += newTaskHTML
+}
 
-//
-//
-// });
-//
-// document.addEventListener('click', (e) => {
-//   if (e.target.className === "delete-list") {
-//     e.target.parentElement.parentElement.remove()
-//     divContent.innerHTML = taskFormHTML;
-//   }
-// })
+
+document.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (e.target.className === "delete-list") {
+    e.target.parentElement.parentElement.remove()
+    App.checkRenderTask();
+  }
+})
